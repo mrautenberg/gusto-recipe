26e9dea853a8e66d0b20559c7fc892104910aa46
Max Rautenberg <mrautenberg@outlook.com>
2021-10-01 10:22:36 +0200

    :construction: navbar in place, WIP on styling and func

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,35 +1,157 @@
-import Link from "next/link"
-import ArrowBackIcon from "@mui/icons-material/ArrowBack"
-import AppBar from "@mui/material/AppBar"
+import * as React from "react"
+import { styled, useTheme } from "@mui/material/styles"
 import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
 import Button from "@mui/material/Button"
+import Drawer from "@mui/material/Drawer"
+import MuiAppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import CssBaseline from "@mui/material/CssBaseline"
+import List from "@mui/material/List"
+import Typography from "@mui/material/Typography"
+import Divider from "@mui/material/Divider"
 import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import ChevronRightIcon from "@mui/icons-material/ChevronRight"
+import ListItem from "@mui/material/ListItem"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import InboxIcon from "@mui/icons-material/MoveToInbox"
+import MailIcon from "@mui/icons-material/Mail"
+import Grid from "@mui/material/Grid"
+
+const drawerWidth = 240
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+  })
+)
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}))
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}))
 
 export default function Header() {
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <header>
-      <nav>
-        <Box sx={{ flexGrow: 1, flex: 1, marginBottom: "7rem" }}>
-          <AppBar position="fixed">
-            <Toolbar>
-              <Button color="inherit">
-                <ArrowBackIcon />
-              </Button>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          {/* <Typography variant="h6"  sx={{  }} component="div">
+            <ChevronLeftIcon />
+          </Typography> */}
+          <Grid container>
+            <Grid item xs={2}>
               <IconButton
-                size="large"
-                edge="start"
                 color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
+                aria-label="open drawer"
+                edge="start"
+                onClick={() => console.log("go back")}
+                sx={{ flexGrow: 1, ...(open && { display: "none" }) }}
+              >
+                <ChevronLeftIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={8} />
+            <Grid item xs={2}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="end"
+                onClick={handleDrawerOpen}
+                sx={{ ...(open && { display: "none" }) }}
               >
                 <MenuIcon />
               </IconButton>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </nav>
-    </header>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Main open={open}>
+        <DrawerHeader />
+      </Main>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Home", "Pantry", "Settings", "Recipes", "Sign Out"].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
+      </Drawer>
+    </Box>
   )
 }
