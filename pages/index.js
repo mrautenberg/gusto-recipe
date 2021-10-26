edd611c4c9bcbdb95ade592482a32a3f7954ec10
Max Rautenberg <mrautenberg@outlook.com>
2021-10-26 14:21:09 +0200

    some formatting fixes

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,92 @@
 import { API_URL } from "@/config/index"
-import Link from "next/link"
 
 import Layout from "@/components/Layout/Layout"
 import RecipeCard from "@/components/Recipe/RecipeCard"
 import Search from "@/components/Search"
 
 import Box from "@mui/material/Box"
+import IconButton from "@mui/material/IconButton"
+import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
-/**
- *  @TODO: Create a more dashboard/landing page layout
- *            - Search recipe in focus
- *            - Add to pantry/see pantry
- *            - Latest recipes/treding
- *            - News from Gusto
- */
+import FacebookIcon from "@mui/icons-material/Facebook"
+import InstagramIcon from "@mui/icons-material/Instagram"
+import TwitterIcon from "@mui/icons-material/Twitter"
+import YouTubeIcon from "@mui/icons-material/YouTube"
 
-export default function HomePage({ recipes }) {
+export default function HomePage({ descendingRecipes, ascendingRecipes }) {
   return (
     <Layout>
-      <Typography variant="h3" component="h1">
+      <Typography
+        variant="h3"
+        component="h1"
+        style={{ marginBottom: "1rem" }}
+      >
         Welcome to Gusto
       </Typography>
       <Search />
-      <Typography variant="h5" component="h2">
-        Check out our latest recipes
+      <Typography
+        variant="h5"
+        component="h2"
+        style={{ margin: "1rem 0" }}
+      >
+        Nya recept
       </Typography>
-      {recipes.length === 0 && (
+      {ascendingRecipes.length === 0 && (
         <Typography variant="h6" component="h3">
           No recipes to show
         </Typography>
       )}
       <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={3}>
-        {recipes.map((rcp) => (
+        {ascendingRecipes.map((rcp) => (
           <RecipeCard key={rcp.id} rcp={rcp} />
         ))}
       </Box>
-      <Typography variant="h5" component="h2">
-        Do you need to add something to your pantry?{" "}
-        <Link href="/account/pantry">Go to your pantry</Link>
+      <Typography
+        variant="h5"
+        component="h2"
+        style={{ margin: "1rem 0" }}
+      >
+        Trendar på Gusto
       </Typography>
-      <Typography>
-        Looking for something else? <Link href="/recipes">See all recipes</Link>
-      </Typography>
+      {descendingRecipes.length === 0 && (
+        <Typography variant="h6" component="h3">
+          No recipes to show
+        </Typography>
+      )}
+      <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={3}>
+        {descendingRecipes.map((rcp) => (
+          <RecipeCard key={rcp.id} rcp={rcp} />
+        ))}
+      </Box>
+      <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
+      <IconButton aria-label="Facebook" size="large">
+        <FacebookIcon fontSize="inherit" />
+      </IconButton>
+      <IconButton aria-label="Instagram" size="large">
+        <InstagramIcon fontSize="inherit" />
+      </IconButton>
+      <IconButton aria-label="Twitter" size="large">
+        <TwitterIcon fontSize="inherit" />
+      </IconButton>
+      <IconButton aria-label="Youtube" size="large">
+        <YouTubeIcon />
+      </IconButton>
+    </Stack>
     </Layout>
   )
 }
 
 export async function getServerSideProps() {
   // Limit to two in showcase instead of PER_PAGE
-  const res = await fetch(`${API_URL}/recipes?_sort=title:ASC&_limit=2`)
-  const recipes = await res.json()
+  // @TODO: Bad naming and way of fetching needs fixes
+  const ascendingRes = await fetch(`${API_URL}/recipes?_sort=title:ASC&_limit=2`)
+  const ascendingRecipes = await ascendingRes.json()
+
+  const descendingRes = await fetch(`${API_URL}/recipes?_sort=title:DESC&_limit=2`)
+  const descendingRecipes = await descendingRes.json()
 
   return {
-    props: { recipes },
+    props: { ascendingRecipes, descendingRecipes },
   }
 }
